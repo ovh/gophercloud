ba9cc673bc02cae4cf9fb1b867ae74ae64c3f9cb
Rémi Métral-Charvet <remi.metral-charvet@corp.ovh.com>
2022-01-14 10:41:10 +0000

    add network_type and segmentation_id to network creation options

--- openstack/networking/v2/networks/requests.go
+++ openstack/networking/v2/networks/requests.go
@@ -82,6 +82,8 @@ type CreateOpts struct {
 	TenantID              string   `json:"tenant_id,omitempty"`
 	ProjectID             string   `json:"project_id,omitempty"`
 	AvailabilityZoneHints []string `json:"availability_zone_hints,omitempty"`
+	NetworkType           string   `json:"provider:network_type,omitempty"`
+	SegmentationID        int      `json:"provider:segmentation_id,omitempty"`
 }
 
 // ToNetworkCreateMap builds a request body from CreateOpts.
