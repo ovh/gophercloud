9b9ccc411d6da471934cae0575e490eea87efedc
Benjamin Ziehms <benjaminherlin@gmail.com>
2023-01-16 15:22:35 +0000

    feat(identity): add create, update and delete endpoint group

--- openstack/identity/v3/extensions/endpointgroups/requests.go
+++ openstack/identity/v3/extensions/endpointgroups/requests.go
@@ -76,3 +76,94 @@ func DeleteProjectAssociation(client *gophercloud.ServiceClient, id string, proj
 	_, r.Header, r.Err = gophercloud.ParseResponse(resp, err)
 	return
 }
+
+// CreateOptsBuilder allows extensions to add additional parameters to
+// the Create request.
+type CreateOptsBuilder interface {
+	ToGroupCreateMap() (map[string]interface{}, error)
+}
+
+// CreateOpts provides options used to create a group.
+type CreateOpts struct {
+	// Name is the name of the new endpoint group.
+	Name string `json:"name" required:"true"`
+
+	// Description is a description of the endpoint group.
+	Description *string `json:"description,omitempty"`
+
+	// Filters are the filters of the endpoint group.
+	Filters map[string]interface{} `json:"filters,omitempty"`
+}
+
+// ToGroupCreateMap formats a CreateOpts into a create request.
+func (opts CreateOpts) ToGroupCreateMap() (map[string]interface{}, error) {
+	b, err := gophercloud.BuildRequestBody(opts, "endpoint_group")
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// Create creates a new endpoint group.
+func Create(client *gophercloud.ServiceClient, opts CreateOptsBuilder) (r CreateResult) {
+	b, err := opts.ToGroupCreateMap()
+	if err != nil {
+		r.Err = err
+		return
+	}
+	resp, err := client.Post(createURL(client), &b, &r.Body, &gophercloud.RequestOpts{
+		OkCodes: []int{201},
+	})
+	_, r.Header, r.Err = gophercloud.ParseResponse(resp, err)
+	return
+}
+
+// UpdateOptsBuilder allows extensions to add additional parameters to
+// the Update request.
+type UpdateOptsBuilder interface {
+	ToGroupUpdateMap() (map[string]interface{}, error)
+}
+
+// UpdateOpts provides options for updating an endpoint group.
+type UpdateOpts struct {
+	// Name is the name of the endpoint group.
+	Name string `json:"name,omitempty"`
+
+	// Description is a description of the endpoint group.
+	Description *string `json:"description,omitempty"`
+
+	// Filters are the filters of the endpoint group.
+	Filters map[string]interface{} `json:"filters,omitempty"`
+}
+
+// ToGroupUpdateMap formats a UpdateOpts into an update request.
+func (opts UpdateOpts) ToGroupUpdateMap() (map[string]interface{}, error) {
+	b, err := gophercloud.BuildRequestBody(opts, "endpoint_group")
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// Update updates an existing Endpoint Group.
+func Update(client *gophercloud.ServiceClient, groupID string, opts UpdateOptsBuilder) (r UpdateResult) {
+	b, err := opts.ToGroupUpdateMap()
+	if err != nil {
+		r.Err = err
+		return
+	}
+	resp, err := client.Patch(updateURL(client, groupID), &b, &r.Body, &gophercloud.RequestOpts{
+		OkCodes: []int{200},
+	})
+	_, r.Header, r.Err = gophercloud.ParseResponse(resp, err)
+	return
+}
+
+// Delete deletes an endpoint group.
+func Delete(client *gophercloud.ServiceClient, groupID string) (r DeleteResult) {
+	resp, err := client.Delete(deleteURL(client, groupID), nil)
+	_, r.Header, r.Err = gophercloud.ParseResponse(resp, err)
+	return
+}
